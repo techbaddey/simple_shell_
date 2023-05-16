4aa489591b80af4db2c6892b2df488b951fa7090
cabdala68 <aragasanmo@gmail.com>
2023-05-16 07:37:26 -0700

    init header file

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+void execmd(char **argv);
+char *get_location(char *command);
+
+#endif
